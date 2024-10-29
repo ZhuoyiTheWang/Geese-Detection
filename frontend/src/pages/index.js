2d4aac4cdf9385ad012c3d85cc9090da28a9c9a4
echristianson1020 <eva77c@gmail.com>
2024-10-29 10:01:40 -0600

    Login page-still needs to be hashed.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,7 +1,18 @@
-import LandingPage from "@/components/landingPage";
+import React, { useState } from 'react';
+import LandingPage from '@/components/landingPage';
+import LoginPage from '@/components/loginPage';
 
 export default function Home() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
-    <LandingPage />
+    <>
+      {isAuthenticated ? (
+        <LandingPage />
+      ) : (
+        <LoginPage setIsAuthenticated={setIsAuthenticated} />
+      )}
+    </>
   );
 }
+
