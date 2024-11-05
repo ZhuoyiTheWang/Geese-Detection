e3cad7321d7e9ea0b7d26012d6e386817f7f87a4
rzimmerman21 <rachelzimmerman21@gmail.com>
2024-11-05 08:26:47 -0700

    Created file to convert xml files from goose head database to .txt files for YOLO

---
+++ convert_to_YOLO.py
@@ -0,0 +1,41 @@
+#Need to convert from xml file to file format expected by yolo
+import xmltodict
+import os
+import glob
+
+#Make folder directory for storing images and labels:
+os.makedirs("datasets/images/train", exist_ok=True)
+os.makedirs("datasets/images/validation", exist_ok=True)
+os.makedirs("datasets/labels/train", exist_ok=True)
+os.makedirs("datasets/labels/validation", exist_ok=True)
+
+for xml_file in glob.glob("Data/*.xml"): #for each xml file in dataset downloaded
+    with open(xml_file, 'r') as f: #opening xml file
+        file = xmltodict.parse(f.read()) #convert to dictionary
+        xmin = int((file["annotation"]["object"]["bndbox"]["xmin"])) #access xmin of bounding box
+        ymin = int((file["annotation"]["object"]["bndbox"]["ymin"])) #access ymin of bounding box
+        xmax = int((file["annotation"]["object"]["bndbox"]["xmax"])) #access xmax of bounding box
+        ymax = int((file["annotation"]["object"]["bndbox"]["ymax"])) #access ymax of bounding box
+        w = int((file["annotation"]["size"]["width"])) #access width of photo
+        h = int((file["annotation"]["size"]["height"])) #access height of photo
+
+    x_center_n = (xmin + xmax) / (2 * w) #calculate normalized center of bounding box in x-direction
+    y_center_n = (ymin + ymax) / (2 * h) #calculate normalized center of bounding box in y-direction
+    bbox_w_n = (xmax - xmin) / w #calculate normalized bounding box width
+    bbox_h_n = (ymax - ymin) / h #calculate normalized bounding box height
+
+
+    with open(f'datasets/labels/train/{xml_file[-8:-4]}.txt','w') as f: #create label file
+        f.write(f"0 {x_center_n} {y_center_n} {bbox_w_n} {bbox_h_n}") #write information to file - label = 0 because we're only labeling 1 class (geese)
+    
+
+for image_file in glob.glob("Data/*.jpg"): #for each image file in dataset download
+    source_path = f'{image_file}'
+    destination_path = f'datasets/images/train/{image_file[-8:]}'
+    os.rename(source_path, destination_path)
+
+
+
+
+
+
