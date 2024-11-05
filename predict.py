26fb10dcdce3d8f719b9415ff4137c19bc5b6eb8
rzimmerman21 <rachelzimmerman21@gmail.com>
2024-11-05 10:11:29 -0700

    Added testing of our own images as well

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 model = YOLO("runs/detect/train/weights/best.pt") #load best weights from training
 
-for i, img_file in enumerate(glob.glob("Data/datasets/images/test/*.jpg")): #for image files in testing folder
+for i, img_file in enumerate(glob.glob("datasets/images/test/*.jpg")): #for image files in testing folder
     result = model.predict(img_file)[0] #predict using the image
     result = result.plot(line_width=1) #plot results with line width of 1
     result = result[:, :, ::-1] #switch from BGR to RGB
@@ -14,3 +14,11 @@ for i, img_file in enumerate(glob.glob("Data/datasets/images/test/*.jpg")): #for
     if i >= 5: #to start, only run with 5 test cases
         break #end for loop
 
+for i, img_file in enumerate(glob.glob("Data/testing_images/*.jpg")): #for image files in testing folder
+    result = model.predict(img_file)[0] #predict using the image
+    result = result.plot(line_width=1) #plot results with line width of 1
+    result = result[:, :, ::-1] #switch from BGR to RGB
+    result = Image.fromarray(result) #plot as image
+    result.save(f'output_{img_file[-8:]}') #save image as output with same numerical value
+    if i >= 5: #to start, only run with 5 test cases
+        break #end for loop
