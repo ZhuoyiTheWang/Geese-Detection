62fcbf0171953e38b24868ac8addb18ff85854df
rzimmerman21 <rachelzimmerman21@gmail.com>
2024-11-05 10:07:48 -0700

    Created file to test model using 5 images from testing

---
+++ predict.py
@@ -0,0 +1,16 @@
+#FILE PURPOSE: test model trained on testing data
+from ultralytics import YOLO
+import glob
+from PIL import Image
+
+model = YOLO("runs/detect/train/weights/best.pt") #load best weights from training
+
+for i, img_file in enumerate(glob.glob("Data/datasets/images/test/*.jpg")): #for image files in testing folder
+    result = model.predict(img_file)[0] #predict using the image
+    result = result.plot(line_width=1) #plot results with line width of 1
+    result = result[:, :, ::-1] #switch from BGR to RGB
+    result = Image.fromarray(result) #plot as image
+    result.save(f'output_{img_file[-8:]}') #save image as output with same numerical value
+    if i >= 5: #to start, only run with 5 test cases
+        break #end for loop
+
