d05f70894c7f2074b5ebb46df3addfbb7a8fba3a
rzimmerman21 <rachelzimmerman21@gmail.com>
2024-11-12 09:14:39 -0700

    Updated data_download to include downloading lionhead goose dataset

--- data_download.py
+++ data_download.py
@@ -2,8 +2,9 @@
 #Includes image files, pnt files, and JSON files
 
 import gdown
-from git import Repo
+from git import repo
 import os
+import requests
 
 def download_from_gdrive() -> None:
     """Downloads all data from the google drive into the Data folder"""
@@ -27,9 +28,30 @@ def download_goose_mugshots() -> None:
     local_dir = 'Data/goose-dataset'
 
     # Clone the repository
-    Repo.clone_from(repo_url, local_dir)
+    repo.clone_from(repo_url, local_dir)
     
     print("Dataset downloaded to Data/goose-dataset")
+
+def download_lionhead_goose() -> None:
+    """Checks out the git with all of the goose mugshots. Data can be found at Data/goose-dataset/{images/annotations}."""
+
+    # check if repository is already there
+    if os.path.isdir("Data/lionhead_data/images") and os.path.isdir("Data/lionhead_data/annotations"):
+        print("Data already exists. No action needed.")
+        return
+
+    # URL of the file download
+    url = 'https://download.scidb.cn/download?fileId=928daf13e1010ac06d9e52797e570512&path=/V3/Small_Goose.zip&fileName=Small_Goose.zip'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        with open('Data/lionhead_data', 'wb') as folder:
+            folder.write(response.content)
+            print("Dataset downloaded to Data/lionhead_data")
+    else:
+        print('Failed to download file.')
+    
+    
 
 
 if __name__ == "__main__":
@@ -37,3 +59,4 @@ if __name__ == "__main__":
     # git download
     download_from_gdrive()
     download_goose_mugshots()
+    download_lionhead_goose()
