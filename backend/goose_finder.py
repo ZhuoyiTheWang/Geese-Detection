74c5001cc505a324b3c56a60b3504d9ace68fda4
rzimmerman21 <rachelzimmerman21@gmail.com>
2024-11-19 10:48:04 -0700

    Updated output file naming convention in goose_finder and created list of output file names as function return parameter

--- backend/goose_finder.py
+++ backend/goose_finder.py
@@ -4,15 +4,15 @@
 from ultralytics import YOLO
 import glob
 from PIL import Image
-import re
 
 
 def count_geese(img_list):
     model = YOLO("./Model/best.pt") #load best weights from training
 
     counts = [] #create list for counts
+    output_image_names = [] #create list of output image names
 
-    for img in img_list: #iterate through images
+    for i, img in enumerate(img_list): #iterate through images
         result = model.predict(img)[0] #predict using the image
 
         counts.append(len(result.boxes)) #add the number of geese counted to the list
@@ -20,11 +20,13 @@ def count_geese(img_list):
         result = result.plot(line_width=1) #plot results with line width of 1
         result = result[:, :, ::-1] #switch from BGR to RGB
         result = Image.fromarray(result) #plot as image
-        #just get digits of file without other characters
-        file_digits = re.search(r'(\d+)\.jpg', img).group(1)
-        result.save(f'OutputImages/output_{file_digits}.jpg') #save image as output with same numerical value
+        
+        result.save(f'OutputImages/output_{i}.jpg') #save image as output with same numerical value
+        output_image_names.append(f'OutputImages/output_{i}.jpg') #add name to list of output file names
 
-    return counts
+    return counts, output_image_names
+
+
 
 
 
