262b668588e3cc83c98774772ce8737509912093
rzimmerman21 <rachelzimmerman21@gmail.com>
2025-02-06 11:32:52 -0700

    Added color-coding features to confusion plot. Reformatted histogram with more useful features

--- backend/model_analysis.py
+++ backend/model_analysis.py
@@ -5,6 +5,8 @@ import glob
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas
+from collections import Counter
 
 
 def count_on_labeled_data(model, img_num = None, save = False, show = False): #img_num = number of images to be processed, save = boolean of whether or not files should be saved, show = boolean of whether or not images should be shown
@@ -37,25 +39,52 @@ def count_on_labeled_data(model, img_num = None, save = False, show = False): #i
         if save: #save images if true
             result.save(f'OutputImages/output_{img_file[-8:]}') #save image as output with same numerical value
 
-    x = np.linspace(0, 100, 200) #generate line 
+    x = np.linspace(0, max(max(predicted_counts), max(actual_counts)), 500) #generate line 
     y = x
 
+
     plt.figure()
-    plt.scatter(actual_counts, predicted_counts)
-    plt.plot(x, y, color='red')
+    plt.scatter(actual_counts, predicted_counts, s=50, cmap="jet", c=np.abs(np.array(counts_accuracy)))
+    plt.plot(x, y, color='midnightblue')
     plt.xlabel('Actual count')
     plt.ylabel('Predicted count')
     plt.title('Confusion Plot')
+    cbar = plt.colorbar()
+    cbar.set_label("Error Percentage", rotation=270)
     #plt.savefig('Confusion_plot.png')
     plt.show()
 
     plt.figure()
     #create bins for histogram
-    bins = [-200, -150, -120, -100, -90, -80, -70, -60, -50, -45, -40, -35, -30, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100, 120, 150, 200]
-    plt.hist(x=counts_accuracy, bins=bins, edgecolor='black')
+    counts = np.array([])
+    bins = np.array([])
+    #Create bins for cut-off values:
+    bin_cuttoffs = np.array([-200, -150, -120, -100, -80, -60, -40, -30, -20, -15, -10, -5, 0, 5, 10, 15, 20, 30, 40, 80, 100, 120, 150, 200])
+    for k in range (len(bin_cuttoffs)-1):
+        if (k == 0):
+            bins = np.append(bins, ">-200%") #lowest bin
+        elif (k <= len(bin_cuttoffs)-1): #middle bins
+            bins = np.append(bins, str(bin_cuttoffs[k]) + "%")
+    bins = np.append(bins, ">200%") #highest bin
+    
+    for i in counts_accuracy: #for each accuracy, determine which cut-off it lies between
+        if (i < bin_cuttoffs[0]): #less than the lowest category
+            counts = np.append(counts, 0)
+            continue
+        if (i > bin_cuttoffs[-1]): #greater than the highest category
+            counts = np.append(counts, len(bin_cuttoffs)-1)
+            continue
+        for j in range (len(bin_cuttoffs)-1): #between two bins
+            if (i >= bin_cuttoffs[j] and i < bin_cuttoffs[j + 1]):
+                counts = np.append(counts, j)
+                break
+
+    #PLOTTING:
+    n, bins_i, patches, = plt.hist(counts, bins=range(len(bin_cuttoffs)), edgecolor='b')
+    plt.xticks(ticks=range(len(bins)), labels=bins, rotation=45, ha='right')
     plt.xlabel('Error Percentage')
     plt.ylabel('Frequency')
-    plt.vlines(x=0, ymin=0, ymax=5, colors='red')
+    plt.vlines(x=(len(bins)/2), ymin=0, ymax=max(n), colors='red')
     plt.title('Error Frequency')
     #plt.savefig('Error_histogram.png')
     plt.show()
@@ -81,4 +110,4 @@ if __name__ == "__main__":
 
     model = YOLO("Model/custom_150_no_opt_best.pt") #load best weights from training
 
-    counts_accuracy, file_names, actual_counts, predicted_counts = count_on_labeled_data(model, img_num=5, save=False, show=True)
+    counts_accuracy, file_names, actual_counts, predicted_counts = count_on_labeled_data(model, save=False, show=False)
