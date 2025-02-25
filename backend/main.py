9807ed565104fef8167a424e426b8053285e3661
Zhuoyi-The-Wang <109244989+Zhuoyi-The-Wang@users.noreply.github.com>
2025-02-25 00:51:30 -0700

    Roll back

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 # Add CORS middleware to allow requests from the React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://goose.minigathering.com"],  # Allow only your React app to access the API
+    allow_origins=["https://goose.minigathering.com", "http://localhost:3000"],  # Allow only your React app to access the API
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
