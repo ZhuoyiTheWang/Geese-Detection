67fc459a93ae4947991774d205543e2d2a60a984
Zhuoyi-The-Wang <109244989+Zhuoyi-The-Wang@users.noreply.github.com>
2025-03-04 11:53:40 -0700

    install python packages in setup

--- .platform/hooks/prebuild/01_setup.sh
+++ .platform/hooks/prebuild/01_setup.sh
@@ -4,6 +4,6 @@ sudo mkdir -p /mnt/large_tmp
 sudo chmod 1777 /mnt/large_tmp
 
 # Ensure TMPDIR is globally set for all processes
-echo "export TMPDIR=/mnt/large_tmp" | sudo tee -a /etc/profile.d/tmpdir.sh
-sudo chmod +x /etc/profile.d/tmpdir.sh
+echo "export TMPDIR=/mnt/large_tmp"
 export TMPDIR=/mnt/large_tmp
+/var/app/venv/staging-LQM1lest/bin/pip install --no-cache-dir -r /var/app/staging/requirements.txt
