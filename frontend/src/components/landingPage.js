3cde710e84029b1eccd80ee85ee6ff784e6f5589
echristianson1020 <eva77c@gmail.com>
2025-03-13 10:36:36 -0600

    download button

--- frontend/src/components/landingPage.js
+++ frontend/src/components/landingPage.js
@@ -337,15 +337,24 @@ export default function LandingPage() {
                 alignItems: 'center',
                 width: '100%',
                 height: '14vh',
+                gap: '5px',
               }}
             >
               <Button
                 variant="contained"
                 color="success"
                 onClick={handleCountClick}
-                sx={{ fontSize: '1.5rem', width: '90%', height: '50%' }}
+                sx={{ fontSize: '1.5rem', width: '48%', height: '50%' }}
               >
                 Count
+              </Button>
+              <Button
+                variant="contained"
+                color="warning"
+                //onClick={handleDownloadClick}
+                sx={{ fontSize: '1.5rem', width: '48%', height: '50%' }}
+              >
+                Download
               </Button>
             </Box>
           </Grid>
@@ -445,7 +454,7 @@ export default function LandingPage() {
     <div>
       <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
         <TitleComponent />
-        <Grid container spacing={2} sx={{ margin: '20px', marginTop: '20px', height: '100%' }}>
+        <Grid container spacing={2} sx={{ margin: '20px', marginTop: '5px', height: '100%' }}>
           <Grid size={9}>
             <ImageTable entries={entries} onEntryClick={showImage} />
           </Grid>
@@ -456,7 +465,7 @@ export default function LandingPage() {
                 variant="contained"
                 onClick={handleDialogOpen}
                 fullWidth
-                sx={{ height: '12vh', fontSize: '2rem', marginBottom: '20px' }}
+                sx={{ height: '8vh', fontSize: '2rem', marginBottom: '15px' }}
               >
                 Add Entry
               </Button>
@@ -465,9 +474,18 @@ export default function LandingPage() {
                 onClick={handleCountClick}
                 fullWidth
                 color="success"
-                sx={{ height: '12vh', fontSize: '2rem', marginBottom: '20px' }}
+                sx={{ height: '8vh', fontSize: '2rem', marginBottom: '15px' }}
               >
                 Count
+              </Button>
+              <Button
+                variant="contained"
+                //onClick={handleDownloadClick}
+                fullWidth
+                color="warning"
+                sx={{ height: '8vh', fontSize: '2rem', marginBottom: '15px' }}
+              >
+                Download
               </Button>
               <TotalTable parkTotals={calculateParkTotals(entries, parks)} />
             </Box>
